d7d0b8874897711f4e6ea2031ee7e5ac72e78cab
roxma <roxma@qq.com>
2017-06-25 14:54:35 +0800

    add github repo completion

--- pythonx/cm_sources/github_repo.py
+++ pythonx/cm_sources/github_repo.py
@@ -1,29 +1,39 @@
 
 from cm import register_source, Base, getLogger
-register_source(name='github-user',
-                   abbreviation='user',
+register_source(name='github-repo',
+                   abbreviation='repo',
                    scopes=['gitcommit', 'markdown', 'magit'],
-                   word_pattern = r'[a-zA-Z_]+',
+                   word_pattern = r'[a-zA-Z_\-]+',
                    cm_refresh_length=-1,
-                   cm_refresh_patterns=[r'github.com\/', r'@'],
+                   cm_refresh_patterns=[r'\b(\w+)\/'],
                    priority=9)
 
 from urllib.request import urlopen
 from urllib.parse import urlencode
 import json
+import re
 
 logger = getLogger(__name__)
 
 class Source(Base):
 
-    def __init__(self,nvim):
+    def __init__(self, nvim):
         Base.__init__(self, nvim)
 
-    def cm_refresh(self,info,ctx):
+    def cm_refresh(self, info, ctx):
+
+        # `.*` greedy match, push to the the end
+        muser = re.search(r'.*\b(\w+)\/', ctx['typed'][ : -len(ctx['base'])])
+        if not muser:
+            logger.debug("match user string failed")
+            return
+
+        user = muser.group(1)
         query = {
-                'q': ctx['base'] + ' in:login',
+                'q': ctx['base'] + ' in:name user:' + user,
                 }
-        url = 'https://api.github.com/search/users?' + urlencode(query)
+
+        url = 'https://api.github.com/search/repositories?' + urlencode(query)
         logger.debug("url: %s", url)
 
         matches = []
@@ -32,7 +42,7 @@ class Source(Base):
             logger.debug("rsp: %s", rsp)
             rsp = json.loads(rsp.decode())
             for item in rsp['items']:
-                matches.append(item['login'])
+                matches.append(dict(word=item['name'], menu=item['full_name']))
 
         logger.debug("matches: %s", matches)
         self.complete(info, ctx, ctx['startcol'], matches, refresh=rsp['incomplete_results'])
